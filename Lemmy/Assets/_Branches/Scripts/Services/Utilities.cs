92a0f91afbb973cf71de961a430afd262ec15b7f
“zongzi” <1320294729@qq.com>
2023-12-27 17:26:56 +0800

    init

---
+++ Lemmy/Assets/_Branches/Scripts/Services/Utilities.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+
+namespace SgLib
+{
+    public static class Utilities
+    {
+        public static IEnumerator CRWaitForRealSeconds(float time)
+        {
+            float start = Time.realtimeSinceStartup;
+
+            while (Time.realtimeSinceStartup < start + time)
+            {
+                yield return null;
+            }
+        }
+
+        public static void ButtonClickSound()
+        {
+            SoundManager.Instance.PlaySound(SoundManager.Instance.button);
+        }
+
+        // Opens a specific scene
+        public static void GoToScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public static void RateApp()
+        {
+            switch (Application.platform)
+            {
+                case RuntimePlatform.IPhonePlayer:
+                    Application.OpenURL(AppInfo.Instance.APPSTORE_LINK);
+                    break;
+        			
+                case RuntimePlatform.Android:
+                    Application.OpenURL(AppInfo.Instance.PLAYSTORE_LINK);
+                    break;
+            }
+        }
+
+        public static void ShowMoreGames()
+        {
+            switch (Application.platform)
+            {
+                case RuntimePlatform.IPhonePlayer:
+                    Application.OpenURL(AppInfo.Instance.APPSTORE_HOMEPAGE);
+                    break;
+    			
+                case RuntimePlatform.Android:
+                    Application.OpenURL(AppInfo.Instance.PLAYSTORE_HOMEPAGE);
+                    break;
+            }
+        }
+
+        public static void OpenFacebookPage()
+        {
+            Application.OpenURL(AppInfo.Instance.FACEBOOK_LINK);
+        }
+
+        public static void OpenTwitterPage()
+        {
+            Application.OpenURL(AppInfo.Instance.TWITTER_LINK);
+        }
+
+        public static void ContactUs()
+        {
+            string email = AppInfo.Instance.SUPPORT_EMAIL;
+            string subject = EscapeURL(AppInfo.Instance.APP_NAME + " [" + Application.version + "] Support");
+            string body = EscapeURL("");
+            Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+        }
+
+        public static string EscapeURL(string url)
+        {
+            return WWW.EscapeURL(url).Replace("+", "%20");
+        }
+
+        public static int[] GenerateShuffleIndices(int length)
+        {
+            int[] array = new int[length];
+
+            // Populate array
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            // Shuffle
+            for (int j = 0; j < array.Length; j++)
+            {
+                int tmp = array[j];
+                int randomPos = Random.Range(j, array.Length);
+                array[j] = array[randomPos];
+                array[randomPos] = tmp;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Lemmy/Assets/_Branches/Scripts/UIManager.cs
@@ -0,0 +1,274 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using SgLib;
+
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Object References")]
+    public GameManager gameManager;
+    public GameObject header;
+    public Text score;
+    public Text bestScore;
+    public Text coinText;
+    public Text title;
+    public GameObject tapToStart;
+    public GameObject menuButtons;
+    public Text dailyRewardBtnText;
+    public GameObject rewardUI;
+    public GameObject settingsUI;
+    public GameObject soundOnBtn;
+    public GameObject soundOffBtn;
+
+    
+    [Header("In-App Purchase Store")]
+    public GameObject storeUI;
+
+    [Header("Sharing-Specific")]
+    public GameObject shareUI;
+    public Image sharedImage;
+
+    Animator scoreAnimator;
+    Animator dailyRewardAnimator;
+    bool isWatchAdsForCoinBtnActive;
+
+    void OnEnable()
+    {
+        GameManager.GameStateChanged += GameManager_GameStateChanged;
+        ScoreManager.ScoreUpdated += OnScoreUpdated;
+    }
+
+    void OnDisable()
+    {
+        GameManager.GameStateChanged -= GameManager_GameStateChanged;
+        ScoreManager.ScoreUpdated -= OnScoreUpdated;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreAnimator = score.GetComponent<Animator>();
+        
+        Reset();
+        ShowStartUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = ScoreManager.Instance.Score.ToString();
+        bestScore.text = ScoreManager.Instance.HighScore.ToString();
+        coinText.text = CoinManager.Instance.Coins.ToString();
+
+        
+
+        if (settingsUI.activeSelf)
+        {
+            UpdateMuteButtons();
+        }
+    }
+
+    void GameManager_GameStateChanged(GameState newState, GameState oldState)
+    {
+        if (newState == GameState.Playing)
+        {              
+            ShowGameUI();
+        }
+        else if (newState == GameState.PreGameOver)
+        {
+            // Before game over, i.e. game potentially will be recovered
+        }
+        else if (newState == GameState.GameOver)
+        {
+            Invoke("ShowGameOverUI", 0.5f);
+        }
+    }
+
+    void OnScoreUpdated(int newScore)
+    {
+        scoreAnimator.Play("NewScore");
+    }
+
+    void Reset()
+    {
+        settingsUI.SetActive(false);
+
+        header.SetActive(false);
+        title.gameObject.SetActive(false);
+        score.gameObject.SetActive(false);
+        tapToStart.SetActive(false);
+        menuButtons.SetActive(false);
+        settingsUI.SetActive(false);
+
+        // Enable or disable premium stuff
+        bool enablePremium = gameManager.enablePremiumFeatures;
+        
+        
+        // Hidden by default
+        storeUI.SetActive(false);
+        settingsUI.SetActive(false);
+        shareUI.SetActive(false);
+
+        // These premium feature buttons are hidden by default
+        // and shown when certain criteria are met (e.g. rewarded ad is loaded)
+    }
+
+    public void StartGame()
+    {
+        gameManager.StartGame();
+    }
+
+    public void EndGame()
+    {
+        gameManager.GameOver();
+    }
+
+    public void RestartGame()
+    {
+        gameManager.RestartGame(0.2f);
+    }
+
+    public void ShowStartUI()
+    {
+        settingsUI.SetActive(false);
+
+        header.SetActive(true);
+        title.gameObject.SetActive(true);
+        tapToStart.SetActive(true);
+        
+    }
+
+    public void ShowGameUI()
+    {
+        header.SetActive(true);
+        title.gameObject.SetActive(false);
+        score.gameObject.SetActive(true);
+        tapToStart.SetActive(false);
+    }
+
+    public void ShowGameOverUI()
+    {
+        header.SetActive(true);
+        title.gameObject.SetActive(false);
+        score.gameObject.SetActive(true);
+        tapToStart.SetActive(false);
+        menuButtons.SetActive(true);
+
+        settingsUI.SetActive(false);
+
+        
+        // Hide sharing UI by default
+        shareUI.SetActive(false);
+    }
+
+    
+    
+
+    public void ShowSettingsUI()
+    {
+        settingsUI.SetActive(true);
+    }
+
+    public void HideSettingsUI()
+    {
+        settingsUI.SetActive(false);
+    }
+
+    public void ShowStoreUI()
+    {
+        storeUI.SetActive(true);
+    }
+
+    public void HideStoreUI()
+    {
+        storeUI.SetActive(false);
+    }
+    
+    
+    public void GrabDailyReward()
+    {
+        if (DailyRewardController.Instance.CanRewardNow())
+        {
+            int reward = DailyRewardController.Instance.GetRandomReward();
+
+            // Round the number and make it mutiplies of 5 only.
+            int roundedReward = (reward / 5) * 5;
+
+            // Show the reward UI
+            ShowRewardUI(roundedReward);
+
+            // Update next time for the reward
+            DailyRewardController.Instance.ResetNextRewardTime();
+        }
+    }
+
+    public void ShowRewardUI(int reward)
+    {
+        rewardUI.SetActive(true);
+        rewardUI.GetComponent<RewardUIController>().Reward(reward);
+    }
+
+    public void HideRewardUI()
+    {
+        rewardUI.GetComponent<RewardUIController>().Close();
+    }
+    
+
+    public void HideShareUI()
+    {
+        shareUI.SetActive(false);
+    }
+
+    
+    public void ShowCharacterSelectionScene()
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    public void ToggleSound()
+    {
+        SoundManager.Instance.ToggleMute();
+    }
+
+    public void ToggleMusic()
+    {
+        SoundManager.Instance.ToggleMusic();
+    }
+
+    public void RateApp()
+    {
+        Utilities.RateApp();
+    }
+
+    public void OpenTwitterPage()
+    {
+        Utilities.OpenTwitterPage();
+    }
+
+    public void OpenFacebookPage()
+    {
+        Utilities.OpenFacebookPage();
+    }
+
+    public void ButtonClickSound()
+    {
+        Utilities.ButtonClickSound();
+    }
+
+    void UpdateMuteButtons()
+    {
+        if (SoundManager.Instance.IsMuted())
+        {
+            soundOnBtn.gameObject.SetActive(false);
+            soundOffBtn.gameObject.SetActive(true);
+        }
+        else
+        {
+            soundOnBtn.gameObject.SetActive(true);
+            soundOffBtn.gameObject.SetActive(false);
+        }
+    }
+}
